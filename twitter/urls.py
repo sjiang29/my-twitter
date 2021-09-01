fd79348db5b1c4586ee1b81151acfc8c434e1e86
sj <sjiang29@usfca.edu>
2021-09-01 13:26:58 -0700

    11-comment-create-api

--- twitter/urls.py
+++ twitter/urls.py
@@ -21,6 +21,8 @@ from accounts.api.views import UserViewSet, AccountViewSet
 from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from comments.api.views import CommentViewSet
+
 from django.conf import settings
 
 
@@ -30,6 +32,7 @@ router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships',FriendshipViewSet,basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeed')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 
 urlpatterns = [
